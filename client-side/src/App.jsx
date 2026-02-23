21a4965e30cc5e4888542e4c155a0170ce6a0f46
Asim Rasheed <asimch036@gmail.com>
2026-02-23 20:24:10 +0500

    chnages in app.jsx

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,14 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signin from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
 import AuthLayout from "./pages/AuthLayout";
 function App(){
   return(
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<AuthLayout/>}/>
-      <Route path="/signin" element={<Signin/>}/>
-      <Route path="/signup" element={<SignUp/>}/>
     </Routes>
     </BrowserRouter>
   )
